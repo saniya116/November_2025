a56fc5f57b6a3874b54ded6ec21181cf1fad5652
saniya116 <shaiksaniya6865@gmail.com>
2025-11-06 18:52:33 +0530

    MergeSort.java
    Implement Merge Sort algorithm with user input.

---
+++ mergesort.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+class MergeSort {
+    static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+       while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    static void sort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        sort(arr, 0, n - 1);
+        System.out.println("Sorted array:");
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+
+output:
+Enter size of array:5
+Enter elements:
+5 2 9 1 6
+
+Sorted array:
+1 2 5 6 9
+}
