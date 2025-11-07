1a1f875ae8a8a568573e76b0a18f1d2b9ed93afa
saniya116 <shaiksaniya6865@gmail.com>
2025-11-07 20:22:37 +0530

    bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array:");
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
+
+output:
+Enter size of array:5
+Enter element:
+50
+10
+30
+20
+40
+Sorted array:
+10 20 30 40 50
