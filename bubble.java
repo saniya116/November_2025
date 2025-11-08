f25af2d7131c2cc943590c80b64c171530bac5d2
saniya116 <shaiksaniya6865@gmail.com>
2025-11-08 19:51:33 +0530

    bubble.java

---
+++ bubble.java
@@ -0,0 +1,20 @@
+the theory of Bubble Sort in Java 
+Definition:
+Bubble Sort is a simple comparison-based sorting algorithm.
+It repeatedly goes through the list, compares adjacent elements, and swaps them if they are in the wrong order.
+This process continues until the entire array is sorted.
+Algorithm Steps:
+
+1. Start from the first element of the array.
+
+
+2. Compare the current element with the next element.
+
+
+3. If the current element is greater than the next, swap them.
+
+
+4. Move to the next pair of elements and repeat step 2 and 3 for the rest of the array.
+
+
+5. After each full pass, the **largest element “bubbles
