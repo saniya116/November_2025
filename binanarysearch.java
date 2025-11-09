21abae27c5ef40b4911916dae06fe4a2e8457757
saniya116 <shaiksaniya6865@gmail.com>
2025-11-09 19:55:48 +0530

    binary search in Java
    This Java program implements a binary search algorithm to find an element in a sorted array. It prompts the user for the array size, elements, and the target element to search.

---
+++ binanarysearch.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements in sorted order:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+        int low = 0, high = n - 1, mid, flag = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] == key) {
+                flag = 1;
+                System.out.println("Element found at position: " + (mid + 1));
+                break;
+            } else if (arr[mid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        if (flag == 0)
+            System.out.println("Element not found!");
+    }
+}
+output:
+Enter size of array: 5
+Enter elements in sorted order:
+10 20 30 40 50
+Enter element to search: 30
+Element found at position: 3
