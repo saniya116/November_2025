3c552b7525a720fdcd2dd34631bf973550634f95
saniya116 <shaiksaniya6865@gmail.com>
2025-11-13 21:48:39 +0530

    Implement matrix transposition in Java
    This program takes a matrix as input and calculates its transpose, displaying both the original and transposed matrices.

---
+++ transposematrix.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class TransposeMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+        int[][] matrix = new int[rows][cols];
+        int[][] transpose = new int[cols][rows];
+        System.out.println("\nEnter elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        System.out.println("\nOriginal Matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nTranspose of Matrix:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+output:
+Enter number of rows: 2
+Enter number of columns: 3
+Enter elements of the matrix:
+1 2 3
+4 5 6
+Original Matrix:
+1 2 3
+4 5 6
+Transpose of Matrix:
+1 4
+2 5
+3 6
