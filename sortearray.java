afa8bb067bbdcf2db7706a060fa8f28c6383b55c
saniya116 <shaiksaniya6865@gmail.com>
2025-11-15 20:17:07 +0530

    Add array sorting functionality in SortArray class
    Implement a Java program to sort an array of integers input by the user.

---
+++ sortearray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class SortArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr
+
+output:
+Enter array size: 5
+Enter elements:
+5 3 1 4 2
+Sorted Array:
+1 2 3 4 5
