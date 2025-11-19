b347a1ec32851616a41d313c4b5f9cc7c23ea9e9
saniya116 <shaiksaniya6865@gmail.com>
2025-11-19 18:54:25 +0530

    Fix output message for palindrome check

---
+++ palindromecheck.java
@@ -0,0 +1,17 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        if (str.equals(rev))
+            System.out.println(str + " is a palindrome.");
+        else
+            System.out.println(str + " is not a palindrome.");
+    }
+}
+Output:
+madam is a palindrome.
