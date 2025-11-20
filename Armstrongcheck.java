cc8b86c5c00841d96f610855b976cf446af6b4c8
saniya116 <shaiksaniya6865@gmail.com>
2025-11-20 21:47:16 +0530

    Create Armstrongcheck.java

---
+++ Armstrongcheck.java
@@ -0,0 +1,21 @@
+public class ArmstrongCheck {
+    public static void main(String[] args) {
+        int num = 153;
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is NOT an Armstrong number.");
+        }
+    }
+}
+output:
+153 is an Armstrong number.
