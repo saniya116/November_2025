8f221d46295a5f2809bb6f9e9f93bea8c10c5b0b
saniya116 <shaiksaniya6865@gmail.com>
2025-11-23 22:59:34 +0530

    Create PerfectNumber.java

---
+++ PerfectNumber.java
@@ -0,0 +1,20 @@
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int num = 28;
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == num) {
+            System.out.println(num + " is a Perfect Number");
+        } else {
+            System.out.println(num + " is NOT a Perfect Number");
+        }
+    }
+}
+output:
+28 is a Perfect Number
