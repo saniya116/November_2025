dcbf3dd62b977280c36fd3c4a35a906ebf57a329
saniya116 <shaiksaniya6865@gmail.com>
2025-11-24 20:46:45 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,16 @@
+Explain the concept of Encapsulation in Java.
+Answer:
+
+Encapsulation is one of the main principles of Object-Oriented Programming (OOP) in Java.
+It means wrapping data (variables) and methods (functions) that operate on that data into a single unit called a class.
+The main idea is to protect the data from direct access by using private variables and providing controlled access through public getter and setter methods.
+
+Encapsulation helps in:
+
+Protecting data from unauthorized access
+
+Maintaining security
+
+Making the code easy to modify and maintain
+
+Improving flexibility and reusability
