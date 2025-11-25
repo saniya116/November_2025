93ad6e0b8f08a646ac4d6e7832548675c2c8356d
saniya116 <shaiksaniya6865@gmail.com>
2025-11-25 19:03:30 +0530

    Create Number check.java

---
+++ Number check.java
@@ -0,0 +1,15 @@
+public class NumberChech{
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num > 0)
+            System.out.println(num + " is Positive");
+        else if (num < 0)
+            System.out.println(num + " is Negative");
+        else
+            System.out.println("Zero");
+    }
+}
+
+output:
+-5 is Negative
