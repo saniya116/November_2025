c831cc2dc8ef8e96e0fa910581cbdd77a61ef335
saniya116 <shaiksaniya6865@gmail.com>
2025-11-27 19:10:45 +0530

    Create  A CountVowelsConsonants.java

---
+++ A CountVowelsConsonants.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+        int v = 0, c = 0;
+        for(char ch : str.toCharArray()) {
+            if(ch >= 'a' && ch <= 'z') {
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                    v++;
+                else
+                    c++;
+            }
+        }
+        System.out.println("Vowels: " + v);
+        System.out.println("Consonants: " + c);
+    }
+
+
+Output:
+Enter a string:Sridevi
+Vowels:3
+Consonants:4
