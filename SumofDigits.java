e2f6e20abdeb8736faa4d6be1976a51387680dc8
saniya116 <shaiksaniya6865@gmail.com>
2025-11-29 17:24:16 +0530

    Create SumofDigits.java

---
+++ SumofDigits.java
@@ -0,0 +1,11 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = 12345;
+        int sum = 0;
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("Sum of digits = " + sum);
+    }
+}
